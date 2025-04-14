17a033c8587a059e0928b10c40e16f1064d46305
raimee <electron.devf@gmail.com>
2025-04-15 01:50:39 +0300

    temp: try to display user avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,20 @@
 import 'dart:async';
 
+import 'package:fakelab_records_webapp/core/di/injector.dart';
+import 'package:fakelab_records_webapp/core/domain/providers/telegram_wrapper.dart';
 import 'package:fakelab_records_webapp/core/service/telegram_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-final TelegramService telegramService = TelegramService();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initInjector();
 
-void main() {
-  telegramService.getTelegramData();
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    startTimer();
-    super.initState();
-  }
-
-  double opacity = 1;
-  int dotCount = 1;
-
-  startTimer() {
-    Timer.periodic(const Duration(seconds: 2), (timer) {
-      opacity = opacity == 1 ? 0.6 : 1;
-      setState(() {});
-    });
-    Timer.periodic(const Duration(milliseconds: 500), (timer) {
-      dotCount = dotCount > 2 ? 0 : dotCount;
-      dotCount++;
-      setState(() {});
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,37 +23,38 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xff161616),
-        body: SizedBox(
-          height: size.height,
-          width: size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              AnimatedOpacity(
-                opacity: opacity,
-                duration: const Duration(seconds: 2),
-                curve: Curves.easeInOut,
-                child: const Image(image: AssetImage('assets/images/logo.png')),
-              ),
-              Text(
-                'Разрабатываем${'.' * dotCount}',
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.white70,
-                  fontWeight: FontWeight.w500,
+        body: TelegramDataProvider(
+          builder: (context, data) => SizedBox(
+            height: size.height,
+            width: size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/logo.png'),
+                SvgPicture.network(
+                  data.photoUrl!,
+                  height: 200,
                 ),
-              ),
-              const SizedBox(height: 50),
-              Text(
-                'TG data: ${telegramService.telegramData.toString()}',
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white70,
-                  fontWeight: FontWeight.w500,
+                const Text(
+                  'Разрабатываем...',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 50),
+                Text(
+                  'TG data: ${$<TelegramService>().data.toString()}',
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
