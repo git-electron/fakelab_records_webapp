f79fc859e8ad236c330e3f1708a70b07864d52b1
raimee <electron.devf@gmail.com>
2025-04-15 09:30:42 +0300

    feat: new assets

--- lib/core/gen/assets.gen.dart
+++ lib/core/gen/assets.gen.dart
@@ -19,6 +19,13 @@ class $AssetsIconsGen {
   $AssetsIconsLogoGen get logo => const $AssetsIconsLogoGen();
 }
 
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/coin
+  $AssetsImagesCoinGen get coin => const $AssetsImagesCoinGen();
+}
+
 class $AssetsIconsLogoGen {
   const $AssetsIconsLogoGen();
 
@@ -27,6 +34,20 @@ class $AssetsIconsLogoGen {
 
   /// Directory path: assets/icons/logo/records
   $AssetsIconsLogoRecordsGen get records => const $AssetsIconsLogoRecordsGen();
+}
+
+class $AssetsImagesCoinGen {
+  const $AssetsImagesCoinGen();
+
+  /// File path: assets/images/coin/big.png
+  AssetGenImage get big => const AssetGenImage('assets/images/coin/big.png');
+
+  /// File path: assets/images/coin/small.png
+  AssetGenImage get small =>
+      const AssetGenImage('assets/images/coin/small.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [big, small];
 }
 
 class $AssetsIconsLogoFakelabGen {
@@ -63,6 +84,77 @@ class Assets {
   const Assets._();
 
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName, {this.size, this.flavors = const {}});
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({AssetBundle? bundle, String? package}) {
+    return AssetImage(_assetName, bundle: bundle, package: package);
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
 
 class SvgGenImage {
