01d3be84932f2215e85c055e85764633e0455c8a
raimee <electron.devf@gmail.com>
2025-04-16 05:38:36 +0300

    feat: adaptive appbar

--- lib/presentation/screens/home/presentation/home_screen.dart
+++ lib/presentation/screens/home/presentation/home_screen.dart
@@ -1,11 +1,10 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
 import 'package:auto_route/annotations.dart';
-import 'package:fakelab_records_webapp/core/theme/theme_extensions.dart';
 import 'package:fakelab_records_webapp/presentation/screens/home/presentation/widgets/debug/home_screen_debug_info.dart';
 import 'package:fakelab_records_webapp/presentation/screens/home/presentation/widgets/home_screen_app_bar.dart';
 import 'package:fakelab_records_webapp/presentation/screens/home/presentation/widgets/home_screen_greetings.dart';
 import 'package:fakelab_records_webapp/presentation/screens/home/presentation/widgets/home_screen_summary.dart';
-import 'package:fakelab_records_webapp/presentation/ui/wrappers/safe_area_wrapper.dart';
+import 'package:fakelab_records_webapp/presentation/ui/wrappers/telegram/telegram_meta_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -13,19 +12,21 @@ import 'package:gap/gap.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  static final ScrollController controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: context.colors.background,
-      body: const SafeAreaWrapper(
-        top: true,
-        child: Column(
-          verticalDirection: VerticalDirection.up,
-          children: [
-            Expanded(child: HomeScreenBody()),
-            HomeScreenAppBar(),
-          ],
-        ),
+      body: CustomScrollView(
+        slivers: [
+          TelegramMetaWrapper(builder: (context, meta) {
+            if(meta.isMobile) return const HomeScreenAppBarMobile();
+            return const HomeScreenAppBar();
+          }),
+          const SliverToBoxAdapter(
+            child: HomeScreenBody(),
+          ),
+        ],
       ),
     );
   }
@@ -36,28 +37,25 @@ class HomeScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      clipBehavior: Clip.none,
-      child: Padding(
-        padding: Pad(vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gap(55),
-            HomeScreenDebugInfo(),
-            HomeScreenGreetings(),
-            Gap(20),
-            HomeScreenSummary(),
-            Gap(55),
-            HomeScreenGreetings(),
-            Gap(20),
-            HomeScreenSummary(),
-            Gap(55),
-            HomeScreenGreetings(),
-            Gap(20),
-            HomeScreenSummary(),
-          ],
-        ),
+    return const Padding(
+      padding: Pad(vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Gap(55),
+          HomeScreenDebugInfo(),
+          HomeScreenGreetings(),
+          Gap(20),
+          HomeScreenSummary(),
+          Gap(55),
+          HomeScreenGreetings(),
+          Gap(20),
+          HomeScreenSummary(),
+          Gap(55),
+          HomeScreenGreetings(),
+          Gap(20),
+          HomeScreenSummary(),
+        ],
       ),
     );
   }
--- lib/presentation/screens/home/presentation/widgets/home_screen_app_bar.dart
+++ lib/presentation/screens/home/presentation/widgets/home_screen_app_bar.dart
@@ -3,30 +3,176 @@ import 'package:blur/blur.dart';
 import 'package:fakelab_records_webapp/core/theme/theme_extensions.dart';
 import 'package:fakelab_records_webapp/presentation/ui/logo/logo.dart';
 import 'package:fakelab_records_webapp/presentation/ui/user_avatar/user_avatar.dart';
+import 'package:fakelab_records_webapp/presentation/ui/wrappers/telegram/telegram_meta_wrapper.dart';
 import 'package:flutter/material.dart';
+
+const double _toolbarHeight = 90;
+
+class HomeScreenAppBarMobile extends StatelessWidget {
+  const HomeScreenAppBarMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TelegramMetaWrapper(
+      builder: (context, meta) {
+        final double expandedHeight = _expandedHeightByInsets(
+          meta.safeAreaInset.top,
+          meta.contentSafeAreaInset.top,
+        );
+
+        final double collapsedHeight = _collapsedHeightByInsets(
+          meta.safeAreaInset.top,
+          meta.contentSafeAreaInset.top,
+        );
+
+        return SliverAppBar(
+          expandedHeight: expandedHeight,
+          collapsedHeight: collapsedHeight,
+          toolbarHeight: _toolbarHeight,
+          automaticallyImplyLeading: false,
+          backgroundColor: context.colors.transparent,
+          surfaceTintColor: context.colors.transparent,
+          elevation: 0,
+          scrolledUnderElevation: 0,
+          pinned: true,
+          floating: true,
+          forceElevated: false,
+          flexibleSpace: LayoutBuilder(
+            builder: (context, constraints) {
+              final expandRatio = _calculateExpandRatio(
+                constraints,
+                expandedHeight: expandedHeight,
+                collapsedHeight: collapsedHeight,
+              );
+              final animation = AlwaysStoppedAnimation(expandRatio);
+
+              return Align(
+                alignment: Alignment.bottomCenter,
+                child: Blur(
+                  blur: 10,
+                  blurColor: context.colors.background,
+                  alignment: Alignment.bottomCenter,
+                  overlay: SizedBox(
+                    height: _animate(
+                      animation,
+                      valueWhenCollapsed: meta.contentSafeAreaInset.top,
+                      valueWhenExpanded: _toolbarHeight,
+                    ),
+                    child: Padding(
+                      padding: const Pad(horizontal: 20),
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: _animate(
+                              animation,
+                              valueWhenCollapsed: Alignment.center,
+                              valueWhenExpanded: Alignment.centerLeft,
+                            ),
+                            child: Logo(
+                              height: _animate(
+                                animation,
+                                valueWhenCollapsed: 15,
+                                valueWhenExpanded: 25,
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: _animate(
+                              animation,
+                              valueWhenCollapsed: const Alignment(1.0, -7.0),
+                              valueWhenExpanded: Alignment.centerRight,
+                            ),
+                            child: Opacity(
+                              opacity: _animate(
+                                animation,
+                                valueWhenCollapsed: -5.0,
+                                valueWhenExpanded: 1.0,
+                              ).clamp(0, 1),
+                              child: const UserAvatar(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  child: const SizedBox(
+                    height: double.infinity,
+                    width: double.infinity,
+                  ),
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  T _animate<T>(
+    AlwaysStoppedAnimation<double> animation, {
+    required T valueWhenCollapsed,
+    required T valueWhenExpanded,
+  }) =>
+      Tween<T>(
+        begin: valueWhenCollapsed,
+        end: valueWhenExpanded,
+      ).evaluate(animation);
+
+  double _expandedHeightByInsets(double inset, double contentInset) =>
+      (_toolbarHeight + inset + contentInset)
+          .clamp(_toolbarHeight, double.infinity);
+
+  double _collapsedHeightByInsets(double inset, double contentInset) =>
+      (inset + contentInset).clamp(_toolbarHeight, double.infinity);
+
+  double _calculateExpandRatio(
+    BoxConstraints constraints, {
+    required double expandedHeight,
+    required double collapsedHeight,
+  }) {
+    final double expandRatio = (constraints.maxHeight - collapsedHeight) /
+        (expandedHeight - collapsedHeight);
+
+    return expandRatio.clamp(0, 1);
+  }
+}
 
 class HomeScreenAppBar extends StatelessWidget {
   const HomeScreenAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Blur(
-      blur: 10,
-      blurColor: context.colors.background,
-      overlay: const Padding(
-        padding: Pad(horizontal: 20),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Logo(),
-            UserAvatar(),
-          ],
+    return SliverAppBar(
+      pinned: true,
+      toolbarHeight: _toolbarHeight,
+      automaticallyImplyLeading: false,
+      backgroundColor: context.colors.transparent,
+      surfaceTintColor: context.colors.transparent,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      forceElevated: false,
+      flexibleSpace: Blur(
+        blur: 10,
+        blurColor: context.colors.background,
+        alignment: Alignment.bottomCenter,
+        overlay: const SizedBox(
+          height: _toolbarHeight,
+          child: Padding(
+            padding: Pad(horizontal: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Logo(),
+                UserAvatar(),
+              ],
+            ),
+          ),
         ),
-      ),
-      child: const SizedBox(
-        height: 90,
-        width: double.infinity,
+        child: const SizedBox(
+          height: _toolbarHeight,
+          width: double.infinity,
+        ),
       ),
     );
   }
