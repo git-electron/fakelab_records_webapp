965d56bcf549d73dfec97e70183397ae7c12d897
raimee <electron.devf@gmail.com>
2025-05-04 07:04:21 +0300

    fix: changed initial admin tab

--- lib/presentation/screens/admin/domain/bloc/admin_bloc/admin_state.dart
+++ lib/presentation/screens/admin/domain/bloc/admin_bloc/admin_state.dart
@@ -3,7 +3,7 @@ part of 'admin_bloc.dart';
 @freezed
 class AdminState with _$AdminState {
   const factory AdminState({
-    @Default(AdminTab.orders) AdminTab selectedTab,
+    @Default(AdminTab.general) AdminTab selectedTab,
   }) = _AdminState;
 
   const AdminState._();
