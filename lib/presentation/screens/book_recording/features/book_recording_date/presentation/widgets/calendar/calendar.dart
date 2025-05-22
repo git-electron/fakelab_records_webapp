78c9989894c7fbb656a5910ed9a0408d83fe85b8
raimee <electron.devf@gmail.com>
2025-05-22 17:53:19 +0300

    experiment: own calendar performance with bloc loading bookings

--- lib/presentation/screens/book_recording/features/book_recording_date/presentation/widgets/calendar/calendar.dart
+++ lib/presentation/screens/book_recording/features/book_recording_date/presentation/widgets/calendar/calendar.dart
@@ -8,17 +8,17 @@ class _Calendar extends StatelessWidget {
     final BookRecordingDateBloc bloc = context.read();
     final now = DateTime.now();
 
-    return Column(
-      children: List.generate(3, (index) {
-        final month = DateTime(now.year, now.month + index, now.day);
-        return _OneMonthCalendar2(month);
-      }),
-    );
-
     return BlocBuilder<BookingsBloc, BookingsState>(
       builder: (context, state) {
         return BlocBuilder<BookRecordingDateBloc, BookRecordingDateState>(
           builder: (context, bookRecordingState) {
+            return Column(
+              children: List.generate(3, (index) {
+                final month = DateTime(now.year, now.month + index, now.day);
+                return _OneMonthCalendar2(month);
+              }),
+            );
+
             final DateTime? selectedDay = bookRecordingState.selectedDay;
             return ListView.builder(
               shrinkWrap: true,
