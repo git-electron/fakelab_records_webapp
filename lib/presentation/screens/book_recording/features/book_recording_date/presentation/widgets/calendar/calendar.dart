f0578ae90fb932c592137a6eebb16879d8fb243c
Electron <57866964+git-electron@users.noreply.github.com>
2025-05-22 22:03:34 +0300

    experiment: own calendar with isAvailable predicate performance test (#24)
    * chore: mock and main changed
    
    * feat: book recording screen and booking model
    
    * feat: bool getters
    
    * feat: time selection screen
    
    * feat: duration selection
    
    * fix: duration selection slider
    
    * feat: service images and some refactoring
    
    * refactor: massive amount of refactoring pt. 1
    
    * refactor: massive amount of refactoring pt. 2
    
    * refactor: massive amount of refactoring pt. 3
    
    * refactor: massive amount of refactoring pt. 4
    
    * refactor: massive amount of refactoring pt. 5
    
    * refactor: massive amount of refactoring pt. 6
    
    * refactor: massive amount of refactoring pt. 7
    
    * refactor: massive amount of refactoring pt. 8 (last)
    
    * fix: edit staff member screen
    
    * feat: new booking card on book screen and some refactoring
    
    * feat: booking checkout and checkout feature
    
    * refactor: use new feature in other widgets
    
    * feat: status history feature
    
    * experiment: calendar
    
    * experiment: my own calendar template performance
    
    * experiment: own calendar performance with bloc loading bookings
    
    * experiment: own calendar with isAvailable predicate performance test
    
    ---------
    
    Co-authored-by: raimee <electron.devf@gmail.com>

--- lib/presentation/screens/book_recording/features/book_recording_date/presentation/widgets/calendar/calendar.dart
+++ lib/presentation/screens/book_recording/features/book_recording_date/presentation/widgets/calendar/calendar.dart
@@ -15,48 +15,11 @@ class _Calendar extends StatelessWidget {
             return Column(
               children: List.generate(3, (index) {
                 final month = DateTime(now.year, now.month + index, now.day);
-                return _OneMonthCalendar2(month);
+                return _OneMonthCalendar2(
+                  month: month,
+                  isAvailablePredicate: (date) => state.isDayAvailable(date),
+                );
               }),
-            );
-
-            final DateTime? selectedDay = bookRecordingState.selectedDay;
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                final DateTime focusedDay = DateTime(
-                  now.year,
-                  now.month + index,
-                  now.day,
-                );
-
-                return _OneMonthCalendar2(focusedDay);
-
-                return _OneMonthCalendar(
-                  key: ValueKey(state.bookings),
-                  firstDay: now,
-                  focusedDay: focusedDay,
-                  lastDay: now.add(const Duration(days: 30 * 3)),
-                  selectedDay: selectedDay,
-                  eventLoader: (dateTime) => [dateTime],
-                  // eventLoader: (DateTime dateTime) {
-                  //   return [
-                  //     if (state.isDayAvailable(dateTime) &&
-                  //         !isSameDay(dateTime, now) &&
-                  //         !isSameDay(dateTime, selectedDay))
-                  //       dateTime
-                  //   ];
-                  // },
-                  onDaySelected: (selectedDate, _) {
-                    if (!state.isDayAvailable(selectedDate)) return;
-                    bloc.add(BookRecordingDateEvent.daySelected(selectedDate));
-                    context.pushRoute(BookRecordingTimeRoute(
-                      selectedDate: selectedDate,
-                      bookingsBloc: context.read(),
-                    ));
-                  },
-                );
-              },
             );
           },
         );
