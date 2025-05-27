a1a8cf1e2bd0411885ac525ad39bd0bd0c6c82de
git-electron <electron.devf@gmail.com>
2025-05-27 14:14:46 +0300

    feat: onTap action push to booking screen

--- lib/features/my_bookings/my_bookings_feature.dart
+++ lib/features/my_bookings/my_bookings_feature.dart
@@ -1,8 +1,10 @@
 import 'package:assorted_layout_widgets/assorted_layout_widgets.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:fakelab_records_webapp/core/extensions/border_radius_extensions.dart';
 import 'package:fakelab_records_webapp/core/extensions/color_extensions.dart';
 import 'package:fakelab_records_webapp/core/extensions/datetime_extensions.dart';
 import 'package:fakelab_records_webapp/core/extensions/list_extensions.dart';
+import 'package:fakelab_records_webapp/core/router/router.gr.dart';
 import 'package:fakelab_records_webapp/core/theme/theme_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
